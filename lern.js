58259663c56f393d970832a6c819d871479c2add
Kiborg <yury_kiborg@bk.ru>
2021-05-09 20:43:35 +0300

    variables
    variables from javascript

---
+++ lern.js
@@ -0,0 +1,29 @@
+// let num = 3;
+// alert('num');
+
+let a = 10;
+let b = 2;
+alert(a + b);
+alert(a - b);
+alert(a * b);
+alert(a / b);
+
+// let c = 15;
+// let d = 2;
+// let result = c + d;
+// alert(result);
+
+// let e = 10;
+// let f = 2;
+// let g = 5;
+// alert(e + f + g);
+
+// let h = 17;
+// let j = 10;
+// let k = h - j;
+// alert(k);
+// let l = 8;
+// let result = l + k;
+// alert(result);
+
+
